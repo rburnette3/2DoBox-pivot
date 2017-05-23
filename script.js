8fe09b82df0a1bdf5261b73fb75b969129d7cc9d
David Becker <david@beckersite.com>
2017-05-22 18:08:39 -0600

    Add Local Storage functionality

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-var titleInput = $('#title-input').val();
-var bodyInput = $('#body-input').val();
+// var titleInput = $('#title-input').val();
+// var bodyInput = $('#body-input').val();
 var searchInput = $('#search-input').val();
+var ideaList = [];
+
+function setInLocalStorage() {
+  var stringIdeaList = JSON.stringify(ideaList);
+  localStorage.setItem('ideas', stringIdeaList);
+}
+function getFromLocalStorage() {
+  var getIdeas = localStorage.getItem('ideas')
+  var parseIdeaList = JSON.parse(getIdeas);
+}
+function loadIdeasFromStorage() {
+  if (localStorage.getItem('ideas')) {
+    var getIdeas = localStorage.getItem('ideas')
+    var parseIdeaList = JSON.parse(getIdeas);
+    parseIdeaList.forEach(function(idea) {
+      $('.article-container').prepend(`<article id='${idea.id}'>
+        <div class="description-container">
+          <h2>${idea.title}</h2>
+          <button class="icons" id="delete-btn"></button>
+          <p class="description">${idea.body}</p>
+        </div>
+        <div class="voting-container">
+          <button class="icons" id="upvote-btn"></button>
+          <button class="icons" id="downvote-btn"></button>
+          <p class="quality">quality: <span class="default swill">swill</span></p>
+        </div>
+      </article>`)
+    });
+  } else {
+    console.log('no ideas!!!')
+  }
+}
+
+loadIdeasFromStorage();
 
 $(window).on('keyup', function(e) {
   if(e.keyCode === 13) {
@@ -9,13 +43,19 @@ $(window).on('keyup', function(e) {
   }
 });
 
+function ideaObject(title, body, id) {
+  this.title = title;
+  this.body = body;
+  this.id = id;
+}
 $('#submit-btn').on('click', function(e) {
   e.preventDefault();
   var titleInput = $('#title-input').val();
   var bodyInput = $('#body-input').val();
-  console.log(titleInput);
-  console.log(bodyInput);
-  $('.article-container').prepend(`<article>
+  var titleId = Date.now();
+  ideaList.push(new ideaObject(titleInput, bodyInput, titleId));
+  console.log('idealist', ideaList)
+  $('.article-container').prepend(`<article id='${titleId}'>
     <div class="description-container">
       <h2>${titleInput}</h2>
       <button class="icons" id="delete-btn"></button>
@@ -28,8 +68,15 @@ $('#submit-btn').on('click', function(e) {
     </div>
   </article>`);
   // $('').prop('disabled', true);
+  setInLocalStorage();
   clearInputs();
 });
+
+
+$('.article-container').on('click', '.description', function() {
+  $(this).get(0).contentEditable = "true";
+  $(this).focus();
+})
 
 $('.article-container').on('click', '#upvote-btn', function() {
   if($(this).parent().find('.default').hasClass('swill')) {
@@ -72,7 +119,25 @@ function clearInputs() {
 
 
 $('.article-container').on('click', '#delete-btn', function() {
-  $(this).siblings('article').remove();
+  $(this).parents('article').remove();
   console.log(this);
   console.log('click');
 });
+
+
+$('.article-container').on('click', 'h2', function() {
+  $(this).get(0).contentEditable = "true";
+  $(this).focus();
+  //conditional to check when user clicks outside of h2 or presses enter
+  //then take new value of h2
+  // editedText();
+})
+
+// function editedText() {
+//   $(window).on('click', function() {
+//       var newH2 = $(`#${titleId}`).find('h2').text();
+//       console.log('title id', $(`#${titleId}`))
+//       console.log('new h2', newH2)
+//       console.log('edited Text clicked')
+//    })
+// }
