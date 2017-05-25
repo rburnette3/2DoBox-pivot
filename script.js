b058479acee66fcd3b303d4a31e388887177db0f
Margo Schaedel <margoschaedel@gmail.com>
2017-05-25 12:26:32 -0600

    Refactor and organize script file

--- script.js
+++ script.js
@@ -1,74 +1,60 @@
-loadIdeasFromStorage();
+
+var filteredIdeas = [];
 var ideaList = getFromLocalStorage() || [];
+var indexOfOriginalObject;
+loadIdeasFromStorage();
 
-function setInLocalStorage() {
-  var stringIdeaList = JSON.stringify(ideaList);
-  localStorage.setItem('ideas', stringIdeaList);
-}
-function getFromLocalStorage() {
-  var parseIdeaList = JSON.parse(localStorage.getItem('ideas'));
-  return parseIdeaList
-}
+$('.article-container').on('click', '#delete-btn', removeIdea);
+$('.article-container').on('click', '#downvote-btn', changeDownvoteQuality);
+$('.article-container').on('click', '#upvote-btn', changeUpvoteQuality);
+$('.article-container').on('focusout', '.description', replaceEditedDescription);
+$('.article-container').on('focusout', 'h2', replaceEditedTitle);
 
-function loadIdeasFromStorage() {
-  if (localStorage.getItem('ideas')) {
-    ideaList = getFromLocalStorage();
-    ideaList.forEach(function(idea) {
-      $('.article-container').prepend(`<article id='${idea.id}'>
-        <div class="description-container">
-          <h2 contentEditable = 'true'>${idea.title}</h2>
-          <button class="icons" id="delete-btn"></button>
-          <textarea class="description" contentEditable = 'true'>${idea.body}</textarea>
-        </div>
-        <div class="voting-container">
-          <button class="icons" id="upvote-btn"></button>
-          <button class="icons" id="downvote-btn"></button>
-          <p class="quality">quality: <span class="quality-level">${idea.quality}</span></p>
-        </div>
-      </article>`)
-    });
-  } else {
-    console.log('no ideas!!!')
-  }
-}
-
-$('#submit-btn').on('click', function(e) {
-  e.preventDefault();
-  console.log('submit clicked')
-  // toggleSaveDisable();
-  var titleInput = $('#title-input').val();
-  var bodyInput = $('#body-input').val();
-  var quality = 'swill';
-  var titleId = Date.now();
-  var newIdea = new ideaObject(titleInput, bodyInput, titleId, quality)
-  ideaList.push(newIdea);
-  $('.article-container').prepend(`<article id='${titleId}'>
-    <div class="description-container">
-      <h2 contentEditable = 'true'>${titleInput}</h2>
-      <button class="icons" id="delete-btn"></button>
-      <textarea class="description" contentEditable = 'true'>${bodyInput}</textarea>
-    </div>
-    <div class="voting-container">
-      <button class="icons" id="upvote-btn"></button>
-      <button class="icons" id="downvote-btn"></button>
-      <p class="quality">quality: <span class="quality-level">${newIdea.quality}</span></p>
-    </div>
-  </article>`);
-  setInLocalStorage();
-  clearInputs();
-  filterIdeas();
+$('#body-input').on('input', function() {
+  toggleSaveDisable();
 });
 
+$('#body-input').on('keydown', function(e) {
+  if(e.keyCode === 13) {
+    e.preventDefault();
+  }
+});
 
-function ideaObject(title, body, id, quality) {
-  this.title = title;
-  this.body = body;
-  this.id = id;
-  this.quality = quality;
+$('#search-input').on('input', filterIdeas);
+$('#submit-btn').on('click', submitNewIdea);
+
+$('#title-input').on('input', function() {
+  toggleSaveDisable();
+});
+
+$(window).on('keyup', function(e) {
+  if(e.keyCode === 13 && ($('#title-input').val() !== '') && ($('#body-input').val() !== '')){
+    toggleSaveDisable();
+    $('#submit-btn').trigger('click');
+  }
+});
+
+$(window).on('load', function() {
+  $('#title-input').focus();
+});
+
+function changeDownvoteQuality(e) {
+  var editedObject = findIndexIdeaList($(e.target).parent().parent().prop('id'));
+  localStorage.clear();
+  switchDownvote(editedObject);
+  ideaList.splice(indexOfOriginalObject, 1, editedObject);
+  setInLocalStorage();
+  filterIdeas();
 }
 
-$('.article-container').on('click', '#upvote-btn', changeUpvoteQuality);
-$('.article-container').on('click', '#downvote-btn', changeDownvoteQuality);
+function changeUpvoteQuality(e) {
+  var editedObject = findIndexIdeaList($(e.target).parent().parent().prop('id'));
+  localStorage.clear();
+  switchUpvote(editedObject);
+  ideaList.splice(indexOfOriginalObject, 1, editedObject);
+  setInLocalStorage();
+  filterIdeas();
+}
 
 function clearInputs() {
   $('#title-input').val('');
@@ -77,58 +63,149 @@ function clearInputs() {
   toggleSaveDisable();
 }
 
-$('.article-container').on('click', '#delete-btn', function(e) {
+function displayFilteredList() {
+  $('.article-container').children().remove();
+  filteredIdeas.forEach(function(idea) {
+    prependExistingIdeas(idea);
+  });
+}
+
+function findIndexIdeaList(id) {
+  var list = getFromLocalStorage();
+  var mapIdea = list.map(function(idea) {
+    return idea.id;
+  })
+  var specificID = mapIdea.filter(function(number) {
+    if (parseInt(id) === number) {
+      return number;
+    }
+  })
+  var idAsNumber = specificID[0];
+  var foundObject;
+  list.forEach(function(object, index) {
+    if (parseInt(object.id) === idAsNumber) {
+      foundObject = object;
+      indexOfOriginalObject = index;
+      return object;
+    }
+  })
+  return foundObject;
+}
+
+function filterIdeas() {
+  var searchInput = $('#search-input').val().toUpperCase();
+  ideaList = getFromLocalStorage() || [];
+  if(searchInput === '') {
+    filteredIdeas = [];
+    displayFilteredList();
+    loadIdeasFromStorage();
+  } else {
+      filteredIdeas = ideaList.filter(function(ideaObject) {
+      return ((ideaObject.title.toUpperCase().indexOf(searchInput) > -1) || (ideaObject.body.toUpperCase().indexOf(searchInput) > -1) || (ideaObject.quality.toUpperCase().indexOf(searchInput) > -1))
+    })
+    displayFilteredList();
+  }
+}
+
+function getFromLocalStorage() {
+  var parseIdeaList = JSON.parse(localStorage.getItem('ideas'));
+  return parseIdeaList;
+}
+
+function ideaObject(title, body, id, quality) {
+  this.title = title;
+  this.body = body;
+  this.id = id;
+  this.quality = quality;
+}
+
+function loadIdeasFromStorage() {
+  if (localStorage.getItem('ideas')) {
+    ideaList = getFromLocalStorage();
+    ideaList.forEach(function(idea) {
+      prependExistingIdeas(idea);
+    });
+  } else {
+  }
+}
+
+function prependExistingIdeas(idea) {
+  $('.article-container').prepend(`<article id='${idea.id}'>
+  <div class="description-container">
+  <h2 contentEditable = 'true'>${idea.title}</h2>
+  <button class="icons" id="delete-btn"></button>
+  <p class="description" contentEditable = 'true'>${idea.body}</p>
+  </div>
+  <div class="voting-container">
+  <button class="icons" id="upvote-btn"></button>
+  <button class="icons" id="downvote-btn"></button>
+  <p class="quality">quality: <span class="quality-level">${idea.quality}</span></p>
+  </div>
+  </article>`)
+}
+
+function prependNewIdea(titleId, titleInput, bodyInput, newIdea) {
+  $('.article-container').prepend(`<article id='${titleId}'>
+  <div class="description-container">
+  <h2 contentEditable = 'true'>${titleInput}</h2>
+  <button class="icons" id="delete-btn"></button>
+  <p class="description" contentEditable = 'true'>${bodyInput}</p>
+  </div>
+  <div class="voting-container">
+  <button class="icons" id="upvote-btn"></button>
+  <button class="icons" id="downvote-btn"></button>
+  <p class="quality">quality: <span class="quality-level">${newIdea.quality}</span></p>
+  </div>
+  </article>`);
+}
+
+function removeIdea(e) {
   var editedObject = findIndexIdeaList($(e.target).parent().parent().prop('id'));
-  ideaList.splice(indexOfOriginalObject, 1)
+  ideaList.splice(indexOfOriginalObject, 1);
   localStorage.clear();
   setInLocalStorage();
   $(this).parents('article').remove();
-});
-
-$('.article-container').on('focusout', 'h2', replaceEditedTitle);
-$('.article-container').on('focusout', '.description', replaceEditedDescription);
-
-function replaceEditedTitle(e) {
-  var editedObject = findIndexIdeaList($(e.target).parent().parent().prop('id'));
-  localStorage.clear();
-  editedObject.title = $(this).text()
-  ideaList.splice(indexOfOriginalObject, 1, editedObject)
-  setInLocalStorage();
-  filterIdeas();
 }
 
 function replaceEditedDescription(e) {
   var editedObject = findIndexIdeaList($(e.target).parent().parent().prop('id'));
+  editedObject.body = $(this).text();
+  replaceIdeaInLocalStorage(editedObject);
+}
+
+function replaceEditedTitle(e) {
+  var editedObject = findIndexIdeaList($(e.target).parent().parent().prop('id'));
+  editedObject.title = $(this).text();
+  replaceIdeaInLocalStorage(editedObject);
+}
+
+function replaceIdeaInLocalStorage(editedObject) {
   localStorage.clear();
-  editedObject.body = $(this).text()
-  ideaList.splice(indexOfOriginalObject, 1, editedObject)
+  ideaList.splice(indexOfOriginalObject, 1, editedObject);
   setInLocalStorage();
   filterIdeas();
 }
 
-function changeUpvoteQuality(e) {
-  var editedObject = findIndexIdeaList($(e.target).parent().parent().prop('id'));
-  localStorage.clear();
-  switch (editedObject.quality) {
-    case 'swill':
-      editedObject.quality = 'plausible'
-      $(this).parent().find('.quality-level').text('plausible')
-      break;
-    case 'plausible':
-      editedObject.quality = 'genius'
-      $(this).parent().find('.quality-level').text('genius')
-      break;
-    default:
-      break;
-  }
-  ideaList.splice(indexOfOriginalObject, 1, editedObject)
+function setInLocalStorage() {
+  var stringIdeaList = JSON.stringify(ideaList);
+  localStorage.setItem('ideas', stringIdeaList);
+}
+
+function submitNewIdea(e) {
+  e.preventDefault();
+  var titleInput = $('#title-input').val();
+  var bodyInput = $('#body-input').val();
+  var quality = 'swill';
+  var titleId = Date.now();
+  var newIdea = new ideaObject(titleInput, bodyInput, titleId, quality);
+  ideaList.push(newIdea);
+  prependNewIdea(titleId, titleInput, bodyInput, newIdea);
   setInLocalStorage();
+  clearInputs();
   filterIdeas();
 }
 
-function changeDownvoteQuality(e) {
-  var editedObject = findIndexIdeaList($(e.target).parent().parent().prop('id'));
-  localStorage.clear();
+function switchDownvote(editedObject) {
   switch (editedObject.quality) {
     case 'genius':
       editedObject.quality = 'plausible'
@@ -141,70 +218,22 @@ function changeDownvoteQuality(e) {
     default:
       break;
   }
-  ideaList.splice(indexOfOriginalObject, 1, editedObject)
-  setInLocalStorage();
-  filterIdeas();
 }
 
-// $('.article-container').on('keydown', 'h2', function(e) {
-//   if(e.keyCode === 13) {
-//     $(this).parent().find('.description').focus();
-//   }
-// })
-//
-// $('.article-container').on('keydown', '.description', function(e) {
-//   if(e.keyCode === 13) {
-//     e.preventDefault();
-//     // $('body').focus();
-//     // some way to focus out of the body paragraph
-//   }
-// })
-
-var indexOfOriginalObject;
-
-function findIndexIdeaList(id) {
-  var list = getFromLocalStorage();
-  var mapIdea = list.map(function(idea) {
-    return idea.id;
-  })
-
-  var specificID = mapIdea.filter(function(number) {
-    if (parseInt(id) === number) {
-      return number
-    }
-  })
-  var idAsNumber = specificID[0];
-
-  var foundObject;
-  list.forEach(function(object, index) {
-    if (parseInt(object.id) === idAsNumber) {
-      foundObject = object;
-      indexOfOriginalObject = index;
-      console.log('indexOfOriginal Object: ' + indexOfOriginalObject, 'index: ' + index)
-      return object
-    }
-  })
-  return foundObject
-}
-
-$(window).on('load', function() {
-  $('#title-input').focus();
-})
-
-$(window).on('keyup', function(e) {
-  if(e.keyCode === 13 && ($('#title-input').val() !== '') && ($('#body-input').val() !== '')){
-    toggleSaveDisable();
-    $('#submit-btn').trigger('click');
+function switchUpvote(editedObject) {
+  switch (editedObject.quality) {
+    case 'swill':
+      editedObject.quality = 'plausible'
+      $(this).parent().find('.quality-level').text('plausible')
+      break;
+    case 'plausible':
+      editedObject.quality = 'genius'
+      $(this).parent().find('.quality-level').text('genius')
+      break;
+    default:
+      break;
   }
-});
-
-$('#title-input').on('input', function() {
-  toggleSaveDisable();
-})
-
-$('#body-input').on('input', function() {
-  toggleSaveDisable();
-})
+}
 
 function toggleSaveDisable() {
   var title = $('#title-input').val();
@@ -214,48 +243,4 @@ function toggleSaveDisable() {
   } else {
     $('#submit-btn').prop('disabled', false);
   }
-}
-
-$('#body-input').on('keydown', function(e) {
-  if(e.keyCode === 13) {
-    e.preventDefault();
-  }
-});
-
-$('#search-input').on('input', filterIdeas);
-
-function filterIdeas() {
-  var searchInput = $('#search-input').val().toUpperCase();
-  ideaList = getFromLocalStorage() || [];
-  if(searchInput === '') {
-    filteredIdeas = [];
-    displayFilteredList();
-    loadIdeasFromStorage();
-  } else {
-      filteredIdeas = ideaList.filter(function(ideaObject) {
-      return ((ideaObject.title.toUpperCase().indexOf(searchInput) > -1) || (ideaObject.body.toUpperCase().indexOf(searchInput) > -1) || (ideaObject.quality.toUpperCase().indexOf(searchInput) > -1))
-      console.log('ideaObject in filter fx', ideaObject)
-    })
-    displayFilteredList();
-    console.log('filteredIdeas', filteredIdeas);
-  }
-}
-var filteredIdeas = [];
-function displayFilteredList() {
-  console.log('display filtered list is running')
-  $('.article-container').children().remove();
-  filteredIdeas.forEach(function(idea) {
-    $('.article-container').prepend(`<article id='${idea.id}'>
-      <div class="description-container">
-        <h2 contentEditable = 'true'>${idea.title}</h2>
-        <button class="icons" id="delete-btn"></button>
-        <textarea class="description" contentEditable = 'true'>${idea.body}</textarea>
-      </div>
-      <div class="voting-container">
-        <button class="icons" id="upvote-btn"></button>
-        <button class="icons" id="downvote-btn"></button>
-        <p class="quality">quality: <span class="quality-level">${idea.quality}</span></p>
-      </div>
-    </article>`)
-  });
 }
